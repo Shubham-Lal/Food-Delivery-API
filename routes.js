c0f259e6d0ee9d403a3be470d2be191d037dd4ae
Shubham Lal <im.shubhamlal@gmail.com>
2024-02-04 22:23:07 +0530

    refactor routes

---
+++ routes.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const { calculatePrice } = require("./controllers/price.js");
+
+// Display homepage at "/" (GET request)
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Display Swagger at "/docs"
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Food Delivery API',
+            version: '1.0.0',
+        },
+    },
+    apis: ["./controllers/price.js"],
+};
+const specs = swaggerJsdoc(options);
+router.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, {
+        customSiteTitle: "Food Delivery API",
+        customfavIcon: "/favicon.ico",
+        customCssUrl: [
+            "/swagger-ui.css",
+            "/swagger-dark.css" // for dark theme
+        ],
+        customJs: [
+            "/swagger-ui-bundle.js",
+            "/swagger-ui-standalone-preset.js",
+
+        ]
+    })
+);
+
+// Calculate price at "/api/calculate-price" (POST request)
+router.post("/api/calculate-price", calculatePrice);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,46 +1,20 @@
 const dotenv = require("dotenv");
 const express = require("express");
-const { calculatePrice } = require("./controllers/price.js");
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
 const path = require("path");
+const routes = require('./routes');
 
 dotenv.config();
 
 const server = express();
 server.use(express.json());
 
-server.use(express.static(path.join(__dirname, './public')));
-server.set('views', path.join(__dirname, 'views'));
-server.set('view engine', 'ejs');
-server.get('/', (req, res) => {
-    res.render('index');
-});
-
-const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Food Delivery API',
-            version: '1.0.0',
-        },
-    },
-    apis: ["./controllers/price.js"],
-};
-const specs = swaggerJsdoc(options);
-server.use(
-    "/docs",
-    swaggerUi.serve,
-    swaggerUi.setup(specs, {
-        customSiteTitle: "Food Delivery API",
-        customfavIcon: "/favicon.ico",
-        customCssUrl: ["/swagger-dark.css", "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"]
-    })
-);
-
-server.post("/api/calculate-price", calculatePrice);
-
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`[server]: Server running at PORT => ${port}`);
 });
+
+server.use(express.static(path.join(__dirname, './public')));
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'ejs');
+
+server.use(routes);
